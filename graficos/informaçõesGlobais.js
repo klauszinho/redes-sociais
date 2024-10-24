97bc80ea61c29eb97d72cfbb369247b8ddb6ca03
vulgo everton maozinha <128055802+klauszinho@users.noreply.github.com>
2024-10-24 13:06:18 +0000

    aa

--- graficos/informaçõesGlobais.js
+++ graficos/informaçõesGlobais.js
@@ -1,1 +1,11 @@
-async func
+async function visualizarInformacoesGlobais () {
+    const res = await fetch(url)
+    const dados = await res.json()
+    const paragrafo = document.createElement ('p')
+paragrafo.classList.add('graficos-container_text')
+paragrafo.innerHTML = 'você sabia que o mundo tem
+${dados.total_pessoas_mundo} de pessoas e
+que aproximadamente ${dados.total_pessoas_conectadas}
+estão conectadas em alguma rede social e passam em média
+${dados.tempo_medio} horas conectadas.'
+}
